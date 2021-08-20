66809fc9e1754f8edf840ba9d30381c667b750ff
Craig <craigjope@gmail.com>
2021-08-19 19:59:49 -0600

    commit tree 21f7481e843f22af04c440c0fa41e4dc8529b70f

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,36 @@
 import React from "react";
 import MovieCard from "./MovieCard";
 
+// function App() {
+//   const title = "Mad Max";
+//   const posterURL =
+//     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM";
+//   const genresArr = ["Action", "Adventure", "Science Fiction", "Thriller"];
+
+//   return (
+//     <div className="App">
+//       <MovieCard title={title} genres={genresArr} />
+//     </div>
+//   );
+// }
+
+
+
+// function App() {
+//   const title = "Mad Max";
+//   const posterURL =
+//     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM";
+//   const genresArr = ["Action", "Adventure", "Science Fiction", "Thriller"];
+
+//   return (
+//     <div className="App">
+//       <MovieCard title={title} posterSrc={posterURL} genres={genresArr} />
+//     </div>
+//   );
+// }
+
+
+//using MovieCard default PosterSrc
 function App() {
   const title = "Mad Max";
   const posterURL =
@@ -13,5 +43,7 @@ function App() {
     </div>
   );
 }
+
+
 
 export default App;
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,5 +1,30 @@
+import { getElementError } from "@testing-library/react";
 import React from "react";
 
+//using props
+// function MovieCard(props){
+//   return (
+//   <div className="movie-card">
+//     <img src={props.posterSrc} alt={props.title} />
+//     <h2>{props.title}</h2>
+//     <small>{props.genres.join("! ")}!</small>
+//   </div>
+//   )
+// }
+
+//using destructured props
+// function MovieCard({ title, posterSrc, genres }) {
+//   return (
+//     <div className="movie-card">
+//       <img src={posterSrc} alt={title} />
+//       <h2>{title}</h2>
+//       <small>{genres.join(", ")}</small>
+//     </div>
+//   );
+// }
+
+
+//using destructured props with default values
 function MovieCard({
   title,
   posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
@@ -9,9 +34,11 @@ function MovieCard({
     <div className="movie-card">
       <img src={posterSrc} alt={title} />
       <h2>{title}</h2>
-      <small>{genres.join(", ")}</small>
+      <small>{genres.join("! ")}!</small>
     </div>
   );
 }
+
+
 
 export default MovieCard;
